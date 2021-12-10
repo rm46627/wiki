a7f4a4a6d26e85fbee75751a39a4e8f5d1147e77
rm46627 <michredk@gmail.com>
2021-12-10 17:44:22 +0100

    Docker tests

--- main.go
+++ main.go
@@ -1,25 +1,26 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"log"
 	"net/http"
-	"os"
 
-	"github.com/rm46627/wiki/db"
+	//"os"
+
+	//"github.com/rm46627/wiki/db"
 	"github.com/rm46627/wiki/wiki"
 )
 
 func main() {
 
 	// initializing data base
-	err := db.Initialize()
+	/* err := db.Initialize()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err.Error())
 		os.Exit(1)
 	}
 
-	defer db.Close()
+	defer db.Close() */
 
 	// handling requests from client
 	http.HandleFunc("/", wiki.Handler)
